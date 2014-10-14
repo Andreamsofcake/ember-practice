657c2d5922cc30e65b37918c457068ffb154e89c
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-10-14 11:28:30 -0700

    controlers and templates

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var App = Ember.Application.create({
   LOG_TRANSISITION: true
 });
 
-App.Router.map(function(){
+App.Router.map(function() {
   this.route('about')
+  this.route('insult')
 }); 
+
+App.InsultController = Ember.Controller.extend({
+  insultContent: 'snot nosed wombat'
+});
+
+App.IndexContoller = Ember.Controller.extend({
+  siteName: 'The Bomb'
+});
